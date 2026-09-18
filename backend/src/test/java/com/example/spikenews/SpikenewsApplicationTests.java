5228e7dd4e4cd35ff08e3d12ad7f9686734e793c
Maria Fernanda <mfmsouza@mppr.mp.br>
2026-09-18 12:39:02 -0300

    estrutura inicial

---
+++ backend/src/test/java/com/example/spikenews/SpikenewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spikenews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpikenewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
